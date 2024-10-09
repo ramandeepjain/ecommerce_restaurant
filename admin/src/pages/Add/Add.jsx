74662ace16e78f10a2a35d3a8d9dbceb07514dc2
Ramandeep Jain <ramandeepjain@gmail.com>
2024-10-09 21:07:38 +0200

    admin frontend

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Add.css'
+
+export const Add = () => {
+  return (
+    <div>Add</div>
+  )
+}
---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './List.css'
+
+export const List = () => {
+  return (
+    <div>List</div>
+  )
+}
